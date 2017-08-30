0b6f472f64c75c0501c7aaca0da9b81a45ff44a1
Joshua Needham <joshuabneedham@gmail.com>
2017-08-30 22:18:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,12 @@ if (name === "Susan") {
 } else {
   console.log("Expected the name to be Susan")
 }
+
+if (height === "67") {
+  console.log("The height is correct")
+} else {
+  console.log("Expected the height to 67");
+}
 // Don't worry about this
 module.exports = { name, height
 }
